f77056a0cf031fe994b34311f33bb136e1ae8857
Mark Schenk <m.m.a.schenk@tudelft.nl>
2023-09-29 15:50:16 +0200

    Routing of post to handler with arguments working

--- app.py
+++ app.py
@@ -13,9 +13,10 @@ logging.config.dictConfig(logconfig)
 
 import typing as t
 from apiflask import APIFlask
-from apiflask.fields import Integer
+from apiflask.fields import Integer, String
 import secrets
 import os
+import importlib
 
 from apispec import BasePlugin
 from rabbitlistener import RabbitListener
@@ -44,7 +45,8 @@ class QueuedAPIFlask(APIFlask):
         self.a = "Dinges"
         return super().run(host, port, debug, load_dotenv, **options)
 
-app = QueuedAPIFlask(__name__)
+#app = QueuedAPIFlask(__name__)
+app = APIFlask(__name__)
 app.secret_key = secrets.token_bytes(32)
 
 cache = { 'test': 'value'}
@@ -75,7 +77,8 @@ def _listentries(category):
 
 @app.get("/raw/_entry/<category>/<entry>")
 def _getentry(category, entry):
-    return getentry(category, entry)
+    value = getentry(category, entry)
+    return value
 
 
 @app.post("/cache/<key>/<value>")
@@ -91,19 +94,30 @@ def ledboard(json_data):
     """
     return "OK"
 
-def generategettermappings(queuecollection):
-    for queueinfo in queuecollection:
-        queueid = list(queueinfo.keys())[0]
+fieldmodule = importlib.import_module('apiflask.fields')
 
-        
-        queuesettings = queueinfo[queueid]['mapping']
+def dict2schemadict(map):
+    for key in map.keys():
+        app.logger.debug(f"Setting field type to {map[key]}")        
+        class_ = getattr(fieldmodule, map[key])
+        instance = class_()
+        map[key] = instance
+
+def generategettermappings(queuemanager):
+    queuecollection = queuemanager.getsettings()
+    app.logger.info(f"Found queues {queuecollection}")
+
+    for queueid in queuecollection.keys():
+        queuesettings = queuecollection[queueid]
         
         app.logger.info(f"Registering mappings for {queueid} -> {queuesettings}")
 
-        if 'map' in queuesettings:
-            for map in queuesettings['map']:
+        mappings = queuesettings['mapping'] if 'mapping' in queuesettings else {}
+
+        if 'map' in mappings:
+            for map in mappings['map']:
                 app.logger.debug(f"mapping: {map}")
-                base = f"{queuesettings['base']}/{map['to']}"
+                base = f"{mappings['base']}/{map['to']}"
                 app.logger.debug(f"Mapping from {base} using description {map['description']}")
 
                 def gengetvalue(queueid, entry):
@@ -113,21 +127,63 @@ def generategettermappings(queuecollection):
                         f"""
                         {map['description']}
                         """
-                        app.logger.debug(f"Returning map function for {q} and {e}")
+                        app.logger.debug(f"Returning mapped entry for {q} and {e}")
                         return getentry(q, e)
                     return getvalue
                 
                 implementation = gengetvalue(queueid, map['from'])
                 implementation.__doc__ = map['description']
                 app.add_url_rule( base, map['description'], implementation )
+                
+
+                if 'post' in map and map['post']:
+                    
+                    app.logger.debug(f"map is {map}")
+                    handlefunction = queuemanager.getposthandler(queueid, map)
+                    app.logger.debug(f"Retrieved handlerfunction {handlefunction} that should be {queuemanager.getposthandler(queueid, map)}")
+
+                    def genpostvalue(queueid, entry, thisschema):
+                        q = queueid
+                        e = entry
+                        h = handlefunction
+
+                        #@app.post("/ledboardtest/")
+                        @app.post(base)
+                        @app.input( thisschema, location='json')
+                        def postvalue(json_data=None):
+                            app.logger.debug(f"Posting value [{json_data}] to {q} and {e} ")
+
+                            h(**json_data)
+
+                            return "OK"
+                        return postvalue
+                    
+                    schemadict = map['handlermethod']['args']
+                    dict2schemadict(schemadict)
+
+                    app.logger.debug(f"schemadict is {schemadict}")
+                    
+                    #postimplementation = genpostvalue(queueid, map['from'], { 'aa': Integer(), 'bb': String()})
+                    postimplementation = genpostvalue(queueid, map['from'], schemadict )
+                    postimplementation.__doc__ = map['description']
+
+                    from apiflask.schemas import Schema
+                    from apiflask.scaffold import _annotate
+
+                    schema = Schema.from_dict( {"json_data": Integer() })
+
+                    #app.add_url_rule( base, f"Posting at {map['description']}", postimplementation, methods=['POST'] )
+                    #_annotate(postimplementation, body=schema, body_example=None, body_examples=None)
 
 def setup_app(app, settings):
     app.logger.info("Starting thread")
     queue = Queue()
     app.queue = queue
-    app.cache = RabbitListener(queue=queue, settings=settings['rabbitqueues'])
+    rabbitlistener = RabbitListener(queue=queue, settings=settings['rabbitqueues'])
+    app.listeners = {'rabbitqueues': rabbitlistener }
+    app.cache = rabbitlistener
     app.cache.start()
-    generategettermappings(settings['rabbitqueues'])
+    generategettermappings(rabbitlistener)
     startcachehandler(settings)
     return app
 
@@ -139,7 +195,7 @@ setup_app(app, settings)
 
 def main():
     app.logger.debug("Here we go!")
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
 
 if __name__ == "__main__":
     main()
--- rabbitlistener.py
+++ rabbitlistener.py
@@ -6,37 +6,67 @@ import json
 from logging import getLogger
 from cacher import updatecache
 
+import traceback
+
 logger = getLogger(__name__)
 
+class QueueManager(object):
+    def __init__(self, queue, settings, **kwargs):
+        logger.debug(f"Created queuemanager with {kwargs}")
+        self._queue = queue
+        self._settings = settings
+
+    def getsettings(self):
+        return self._settings
+
 class Handler(object):
-    def __init__(self, id, logger):
+    def __init__(self, id, logger, exchange=None):
         self.id = id
         self.logger = logger
+        self.exchange = exchange
 
     def handlemessage(self, ch, method, properties, body):
         self.logger.info(f"Dropping {body}")
         ch.basic_ack(delivery_tag = method.delivery_tag)
 
-class DropHandler(Handler):
-    pass
+class LEDBoardHandler(Handler):
+    def handlemessage(self, ch, method, properties, body):
+        self.logger.info(f"LEDBoard handler received {body}")
 
-class RFC8428(object):
-    def __init__(self, id, logger):
-        self.id = id
-        self.logger = logger
+        # LedBoard messages:
+        self.logger.debug(f" [{self.id}] Parsing")
+        payload = json.loads(body)
+        self.logger.debug(f" [{self.id}] Parsed")
 
+        if 'type' in payload and 'value' in payload:
+            commandtype = payload['type']
+            commandvalue = { 'value': payload['value'] }
+            self.logger.debug(f"  [{self.id}] Command = {commandtype}. Value = {commandvalue}")
+
+            updatecache(self.id, commandtype, commandvalue)
+
+        self.logger.info(f" [{self.id}] Done")
+
+    def handlepost(self, entry, value ):
+        self.logger.info(f"Handling post value for {entry}: {value}")
+
+    def setActiveState(self, active):
+        self.logger.debug(f'Setting state to {active}')
+
+
+class RFC8428(Handler):
     def handlemessage(self, ch, method, properties, body):
         self.logger.info(f"RFC 8428 handler received {body}")
-        
+
         # RFC8428 Sensor measurement:
-        self.logger.debug(f" [{id}] Parsing")
+        self.logger.debug(f" [{self.id}] Parsing")
         senml = json.loads(body)
-        self.logger.debug(f" [{id}] Parsed")
+        self.logger.debug(f" [{self.id}] Parsed")
         self.logger.debug(f"senml = {senml}")
         ch.basic_ack(delivery_tag = method.delivery_tag)
         bn = None
         for message in senml:
-            self.logger.debug(f" [{id}] Handling message {message}")
+            self.logger.debug(f" [{self.id}] Handling message {message}")
             if 'bn' in message:
                 bn = message['bn']
                 del message['bn']
@@ -44,30 +74,52 @@ class RFC8428(object):
             if 'n' in message:
                 entryid = f"{bn}{message['n']}"
                 message['bn'] = bn
-                self.logger.debug(f" [{id}] Storing at {entryid}: {message}")
-                updatecache(id, entryid, message)
-        self.logger.info(f" [{id}] Done")
+                self.logger.debug(f" [{self.id}] Storing at {entryid}: {message}")
+                updatecache(self.id, entryid, message)
+        self.logger.info(f" [{self.id}] Done")
 
-
-class RabbitListener(object):
-    def __init__(self, queue, settings):
-        self._queue = queue
-        self._settings = settings
+class RabbitListener(QueueManager):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.queues = {}
 
     def start(self):
         logger.debug(f"Starting threads with {self._settings}")
-        for queueinfo in self._settings:
-            queueid = list(queueinfo.keys())[0]
-            queuesettings = queueinfo[queueid]
-            
-            logger.info(f"Starting thread for {queueinfo}")
+        for queueid in self._settings.keys():
+            queuesettings = self._settings[queueid]
+
+            logger.info(f"Starting thread for {queueid}")
             listener_thread = threading.Thread(target=self.readevents, kwargs={'settings': queuesettings, 'id':queueid})
             listener_thread.start()
             logger.info("Thread started")
 
+    def getqueuehandler(self, queueid):
+        logger.debug(f"Searching for handler for {queueid} in {self._settings}")
+        if queueid in self.queues:
+            logger.debug(f"Returning from cache: {self.queues[queueid]}")
+            return self.queues[queueid]
+
+        try:
+            handlerclass = globals()[self._settings[queueid]['handler']]
+            logger.debug(f"handlerclass = {handlerclass}")
+            handler = handlerclass(queueid, logger)
+            logger.debug(f"adding handler = {handler} to queue {self.queues}")
+            self.queues[queueid] = handler
+            logger.debug(f"returning {handler}")
+            return handler
+        except:
+            logger.debug(f"No handler definition for {queueid}. Using self")
+            self.queues[queueid] = self
+            return self
+
+    def getposthandler(self, queueid, mapping):
+        logger.debug(f"Finding post handler for {queueid} and {mapping}")
+        h = getattr(self.getqueuehandler(queueid), mapping['handlermethod']['name'])
+        return h
+
     def handlemessage(self, ch, method, properties, body):
         logger.info(f"default handler received {body}")
-        
+
         # RFC8428 Sensor measurement:
         logger.debug(f" [{id}] Parsing")
         senml = json.loads(body)
@@ -91,8 +143,6 @@ class RabbitListener(object):
 
     def readevents(self, settings, id):
         logger = getLogger(f"rabbitlistener.{id}")
-        
-
 
         mqrabbit_credentials = pika.PlainCredentials(settings['MQRABBIT_USER'], settings['MQRABBIT_PASSWORD'])
         mqparameters = pika.ConnectionParameters(
@@ -104,22 +154,23 @@ class RabbitListener(object):
         channel = mqconnection.channel()
         #channel.exchange_declare(exchange=settings['MQRABBIT_EXCHANGE'])
 
-        queuename = f"{id}-{secrets.token_hex(10)}"
-        result = channel.queue_declare(queue=queuename, exclusive=True )
+        queuename = f"rabbitlistener-{id}-{secrets.token_hex(10)}"
+        result = channel.queue_declare(queue=queuename, exclusive=True, auto_delete=True )
 
         routing_key = settings['MQRABBIT_ROUTINGKEY'] if 'MQRABBIT_ROUTINGKEY' in settings else ""
         exchange = settings['MQRABBIT_EXCHANGE'] if 'MQRABBIT_EXCHANGE' in settings else ""
 
         logger.info(f"Binding queue to exchange: [{exchange}]")
+
         channel.queue_bind(exchange=exchange, queue=result.method.queue, routing_key=routing_key)
 
         if 'handler' in settings:
             handlerclass = globals()[settings['handler']]
-            print(f"handlerclass = {handlerclass}")
+            logger.debug(f"handlerclass = {handlerclass}")
             handler = handlerclass(id, logger)
         else:
             handler = self
-        
+
         channel.basic_consume(queue=result.method.queue, on_message_callback=handler.handlemessage)
         logger.info("Waiting for messages")
         channel.start_consuming()
